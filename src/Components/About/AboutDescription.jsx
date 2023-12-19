cc9daa11a37faefb6f7fe97341add99f7976ae10
JeanEspinoza10 <jeanria10@gmail.com>
2023-12-18 23:01:06 -0500

    upload fotoperfil

--- src/Components/About/AboutDescription.jsx
+++ src/Components/About/AboutDescription.jsx
@@ -1,11 +1,12 @@
 import { AllBox } from "../General/AllBox"
+import Foto_Perfil from '../../img/Foto_Perfil.png'
 
 import '../Styles/styleDescription.css'
 export const AboutDescription = ({contenido}) => {
   return (
     <>
         <section className="contenedorDescripcion">        
-                <AllBox titulo={'Jean Espinoza'} contenido={contenido} img='src\\img\\Foto_Perfil.png'/>
+                <AllBox titulo={'Jean Espinoza'} contenido={contenido} img= {Foto_Perfil}/>
         </section>
     </>
   )
