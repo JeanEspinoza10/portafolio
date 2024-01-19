c5759f4e8de05076daa203ac20e75939dd8bb013
JeanEspinoza10 <jeanria10@gmail.com>
2024-01-19 01:08:27 -0500

    modificated url cv

--- src/Components/General/AllButtons.jsx
+++ src/Components/General/AllButtons.jsx
@@ -4,7 +4,7 @@ export const AllButtons = ({nameButton}) => {
     return (
 
       <>
-        <a href="https://drive.google.com/file/d/1__nhyb5cft-Rc0aDi5S_SEyX_EAhYUMN/view?usp=sharing"  target="_blank" download="CV_Jean Espinoza">
+        <a href="https://drive.google.com/file/d/1QSX64xEreQIOaQ7DKc2euCDE6wn7sesO/view?usp=drive_link"  target="_blank" download="CV_Jean Espinoza">
         <button>{nameButton}</button>  
         </a>  
       </>
