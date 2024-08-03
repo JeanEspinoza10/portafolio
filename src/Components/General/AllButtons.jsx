199a46961fc98c894bbd1f9e58dcea05b73c324b
unknown <jeanria10@gmail.com>
2024-08-03 12:20:43 -0500

    add new link of cv

--- src/Components/General/AllButtons.jsx
+++ src/Components/General/AllButtons.jsx
@@ -4,7 +4,7 @@ export const AllButtons = ({nameButton}) => {
     return (
 
       <>
-        <a href="https://drive.google.com/file/d/1QSX64xEreQIOaQ7DKc2euCDE6wn7sesO/view?usp=drive_link"  target="_blank" download="CV_Jean Espinoza">
+        <a href="https://drive.google.com/file/d/1o5usX3JM_6UOc9jqBOCfnaDdQL5SZNjK/view?usp=sharing"  target="_blank" download="CV_Jean Espinoza">
         <button>{nameButton}</button>  
         </a>  
       </>
