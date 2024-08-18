75f9901aeac895c41a7eb205b3c5ca0cebef90f3
unknown <jeanria10@gmail.com>
2024-08-18 09:31:33 -0500

    fix change styles for responsive

--- src/Components/About/AboutMain.jsx
+++ src/Components/About/AboutMain.jsx
@@ -5,11 +5,11 @@ import { AboutHabilidades } from "./AboutHabilidades";
 
 export const AboutMain = () => {
     
-    const miPresentacion = `Ingeniero mecatrónico con sólidos conocimientos en procesos automatizados, sistemas mecatrónicos y 
-                            lógica de programación. Desde que me sumergí en el mundo de la tecnología, se convirtió en mi pasión. 
-                            Me enfoqué en adquirir conocimientos en desarrollo web, bases de datos y ciencia de datos, 
-                            Los cuales se reflejan en los proyectos personales que he desarrollado. Fortaleciendo mis conocimientos en tecnología; por ello,
-                            estoy accesible a compartir mis conocimientos y explorar colaboraciones.                           
+    const miPresentacion = `Profesional en ingeniera con más de 1 año de experiencia en el área de soporte TI; adquiriendo las
+                            habilidades de trabajo en equipo, empatía y resolución de conflictos. Durante el bachillerato encontré
+                            la pasión de crear soluciones a problemas aplicando la tecnológica, llevándome a estudiar
+                            especializaciones en Desarrollo Web, Base de Datos y Data science. Estoy entusiasmado en aplicar
+                            tanto mis conocimientos técnicos y habilidades blandas en un nuevo proyecto tecnológico.                           
 `;
 
     return (
