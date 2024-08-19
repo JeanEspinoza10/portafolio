249e627ae1d0e89d0a47a94562c66c0ef0922875
unknown <jeanria10@gmail.com>
2024-08-18 21:41:44 -0500

    fix: route of img

--- src/Components/About/AboutHistory.jsx
+++ src/Components/About/AboutHistory.jsx
@@ -5,25 +5,14 @@ import 'react-vertical-timeline-component/style.min.css';
 import python_img from '../../img/python-48.ico'
 import javascript_img from '../../img/javascript.png'
 import django_img from '../../img/django.png'
-import react_img from '../../img/react.png'
-
 import postgresql_img from '../../img/postgresql.png'
-import sqlserver_img from '../../img/servidor-microsoft-sql.png'
-import mongo_img from '../../img/mongo.png'
 import mysql_img from '../../img/mysql.png'
-
-import labview_img from '../../img/labview.png'
-import opencv_img from '../../img/opencv.png'
-import matlab_img from '../../img/matlab.png'
-
-
-import datafactory_img from '../../img/data-factory.png'
-import azure_img from '../../img/azure.png'
-import powerbi_img from '../../img/powerbi.png'
 import flask_png from '../../img/flask.png'
 import service_desk from '../../img/icons/help-desk.png'
 import automata from '../../img/icons/onboarding.png'
 import git from '../../img/icons/git.png'
+import assistant from '../../img/icons/asistente.png'
+import development from '../../img/icons/coding.png'
 
 import '../Styles/styleHistory.css'
 export const AboutHistory = () => {
@@ -35,7 +24,7 @@ export const AboutHistory = () => {
             subtitle: 'COFOPRI',
             description: 'Atender los diferentes requerimientos o incidencias que generaban lo usuarios en el sistema de tickets OTRS.',
             date: '2022',
-            icon_self: 'src\\img\\icons\\asistente.png',
+            icon_self: assistant,
             skills: [service_desk]
         },
         {
@@ -44,7 +33,7 @@ export const AboutHistory = () => {
             subtitle: 'Superintendencia Nacional de Migraciones Perú',
             description: 'Automatizar tickets vía API, categorización y cierre. Reportes con Power BI y Excel. Capacitación y manuales en PROACTIVANET',
             date: '2023',
-            icon_self: 'src\\img\\icons\\asistente.png',
+            icon_self: assistant,
             skills: [python_img,service_desk,automata]
         },
         {
@@ -53,7 +42,7 @@ export const AboutHistory = () => {
             subtitle: 'ixalab',
             description: 'Colaborar con el desarrollo de un MVP, teniendo como framework Django y la aplicación de la metodología SCRUM.',
             date: '2024',
-            icon_self: 'src\\img\\icons\\coding.png',
+            icon_self: development,
             skills: [django_img, mysql_img,git]
         },
         {
@@ -62,7 +51,7 @@ export const AboutHistory = () => {
             subtitle: 'Win Empresas',
             description: 'Desarrollo de APIS con Flask y autenticación con Keycloak. Atender los requerimientos del sistema ODOO. Realizar reportes mediante la conexión a la base de datos PostgreSQL',
             date: '2024 - Present',
-            icon_self: 'src\\img\\icons\\coding.png',
+            icon_self: development,
             skills: [postgresql_img, python_img, flask_png,javascript_img ]
         }
     ]
