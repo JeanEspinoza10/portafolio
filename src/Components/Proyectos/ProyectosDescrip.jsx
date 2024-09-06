745ed55c1ffd6093cf08ef4af569daaf829cb676
unknown <jeanria10@gmail.com>
2024-09-06 00:30:11 -0500

    change footter of page proyect

--- src/Components/Proyectos/ProyectosDescrip.jsx
+++ src/Components/Proyectos/ProyectosDescrip.jsx
@@ -1,73 +1,49 @@
 import { ProyectosAll } from "../helpers/ProyectosAll";
 
-import github_img from '../../img/github.png'
-import youtube_img from '../../img/youtube.png'
+import github_img from "../../img/github.png";
+import youtube_img from "../../img/youtube.png";
 
-import '../Styles/styleProyecto.css'
+import "../Styles/styleProyecto.css";
+ //Elementos
+ const elementosLi = (variableLista) => {
+    return variableLista.map((elemento) => {
+      const despliegue = elemento.link_despliegue || [];
+      const github = elemento.link_github || [];
+      const youtube = elemento.link_youtube || [];
+
+      return (
+        <div key={elemento.id}>
+          <h3>{elemento.titulo}</h3>
+          <p>{`${elemento.descripcion}`}</p>
+          <figure>
+            <a href={despliegue} target="_blank">
+              <img src={elemento.icono_despliegue} alt="" />
+            </a>
+
+            <a href={github} target="_blank">
+              <img className="github" src={github_img} alt="" />
+            </a>
+            <a href={youtube} target="_blank">
+              <img className="github" src={youtube_img} alt="" />
+            </a>
+          </figure>
+        </div>
+      );
+    });
+  };
 
 export const ProyectosDescrip = () => {
+  // Titulo de la sección
+  const titulo = "Proyectos";
 
-    // Titulo de la sección
-    const titulo = 'Proyectos';
-
-    // Obtenemos la data para mostrar
-    const {proyectos} = ProyectosAll();
-
-    //Elementos
-    const elementosLi = (variableLista) => {
-
-        return variableLista.map((elemento)=>{
-            const despliegue = elemento.link_despliegue || [];
-            const github = elemento.link_github || [];
-            const youtube = elemento.link_youtube || [];
-
-
-            
-            
-
-            return(
-                <div key={elemento.id}>
-                    <h3>{elemento.titulo}</h3>
-                    <p>
-                        {
-                            `${elemento.descripcion}`
-                        }
-                    </p>
-                    <figure>
-                        <a href={despliegue} target="_blank">
-                            <img src={elemento.icono_despliegue} alt="" />
-                        </a>
-                        
-                        <a href={github} target="_blank">
-                            
-                            <img className="github" src= {github_img} alt="" />
-                        </a>
-                        <a href={youtube} target="_blank">
-                            
-                            <img className="github" src={youtube_img} alt="" />
-                        </a>
-                    </figure>
-
-
-                </div>
-
-            )
-
-        });
-
-    }
-
+  // Obtenemos la data para mostrar
+  const { proyectos } = ProyectosAll();
   return (
     <>
-        <section className="Proyectos">
-            <h1>{titulo}</h1>
-            <section className="Tarjetas">
-                {
-                    elementosLi(proyectos)
-                }
-            </section>
-
-        </section>
+      <section className="Proyectos">
+        <h1>{titulo}</h1>
+        <section className="Tarjetas">{elementosLi(proyectos)}</section>
+      </section>
     </>
-  )
-}
+  );
+};
