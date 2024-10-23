9c7f9fcc61c11ba3d0f9f64a9707d4f4f029ce79
unknown <jeanria10@gmail.com>
2024-10-22 22:35:09 -0500

    add changes

--- src/Components/General/AllButtons.jsx
+++ src/Components/General/AllButtons.jsx
@@ -4,7 +4,7 @@ export const AllButtons = ({nameButton}) => {
     return (
 
       <>
-        <a href="https://drive.google.com/file/d/1fzbO-iRelD9HqaOpNsv66_aW6GnMrVZK/view?usp=drive_link"  target="_blank" download="CV_Jean Espinoza">
+        <a href="https://drive.google.com/file/d/12swTIYrtNYWISK2X_17UfFRLuesvwIWE/view?usp=drive_link"  target="_blank" download="CV_Jean Espinoza">
         <button>{nameButton}</button>  
         </a>  
       </>
